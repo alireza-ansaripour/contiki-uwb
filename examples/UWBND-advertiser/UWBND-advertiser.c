130a5982d5702c1745616419c8b0c9615db29b54
alireza <alirezaasnaripour73@gmail.com>
2024-07-09 16:14:48 -0500

    added advertiser

--- examples/UWBND-advertiser/UWBND-advertiser.c
+++ examples/UWBND-advertiser/UWBND-advertiser.c
@@ -80,6 +80,12 @@ dwt_config_t config = {
     (SFD_TO) /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
 };
 
+
+dwt_txconfig_t txConf = {
+    0xC9, //PGDelay
+    0x18181818 //30 dB
+};
+
 uint8_t payload[] = {0xad, 0, 0, 0, 0, 0};
 uint8_t rx_payload[20];
 DETECTION_STATUS detection_status = RX_WAK_P1;
@@ -97,75 +103,19 @@ void rx_ok_cb(const dwt_cb_data_t *cb_data){
   dwt_readrxdata(rx_payload, cb_data->datalength, 0);
   dwt_forcetrxoff();
   dwt_rxreset();
-  switch (detection_status){
-  case RX_WAK_P1:
-    detection_status = RX_WAK_P2;
-    break;
-  case RX_WAK_P2:
-    detection_status = CCA;
-    break;
-  
-  case WAITING_TS:
-    if(rx_payload[0] == 0xAA){
-      printf("Received TS MSG\n");
-      detection_status = WAITING;
-    }else{
-      dwt_rxenable(DWT_START_RX_IMMEDIATE);
-    }
-    break; 
-
-  default:
-    break;
-  }
-  
+  dwt_rxenable(DWT_START_RX_IMMEDIATE);  
 }
 
 
 void rx_err_cb(const dwt_cb_data_t *cb_data){
   dwt_forcetrxoff();
   dwt_rxreset();
-  switch (detection_status){
-  case RX_WAK_P1:
-    detection_status = RX_WAK_P2;
-    printf("Detected WaC1: %d\n", node_id);
-    break;
-  case RX_WAK_P2:
-#if (TS_MODE)
-    detection_status = RX_WAK_P3;
-#else
-    detection_status = WAITING;
-#endif
-    printf("Detected WaC2: %d\n", node_id);
-    break;
-  
-  case CCA:
-    detection_status = PKT_DETECTED;
-    break;
-
-  case WAITING_TS:
-    dwt_rxenable(DWT_START_RX_IMMEDIATE);
-    break;
-    
-  default:
-    break;
-  }
+  dwt_rxenable(DWT_START_RX_IMMEDIATE);
 }
 
 
 void rx_to_cb(const dwt_cb_data_t *cb_data){
   dwt_forcetrxoff();
-  switch (detection_status){
-  case RX_WAK_P2:
-    detection_status = RX_WAK_P2;
-    break;
-  case CCA:
-    printf("TX %d\n", node_id);
-    detection_status = RDY_TO_TX;
-    break;
-  default:
-    detection_status = RX_WAK_P1;
-    break;
-  }
 }
 /*---------------------------------------------------------------------------*/
 
@@ -174,6 +124,7 @@ PROCESS_THREAD(range_process, ev, data)
   static struct etimer et;
   static struct etimer timeout;
   static int status;
+  static int T_ADV;
   
 
   PROCESS_BEGIN();
@@ -184,150 +135,23 @@ PROCESS_THREAD(range_process, ev, data)
   }else{
     printf("Failed to set nodeID\n");
   }
-  printf("PAC size 0x%x\n", config.rxPAC);
-  // deployment_print_id_info();
-  etimer_set(&et, CLOCK_SECOND * 1);
-  dwt_configure(&config);
-  PROCESS_WAIT_UNTIL(etimer_expired(&et));
-  dwt_forcetrxoff();
-  dwt_setpreambledetecttimeout(PDTO);  
-  payload[2] = node_id;
-  memcpy(&payload[2], (uint16_t *) &node_id, 2);
-  detection_status = RX_WAK_P1;
+
+
+  T_ADV = 100;
   
 
+  dwt_configure(&config);
+  dwt_configuretxrf(&txConf);
+  dwt_forcetrxoff(); 
+  memcpy(&payload[2], (uint16_t *) &node_id, 2);
+  dwt_writetxdata(sizeof(payload), (uint8_t *) payload, 0);
+  
   while (1){
-    etimer_set(&et, 1);
+    etimer_set(&et, T_ADV);
     PROCESS_WAIT_UNTIL(etimer_expired(&et));
-    if (detection_status == RX_WAK_P1){
-      etimer_set(&et, SNIFF_INTERVAL - 1);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et));
-      config_change = false;
-      if (config.rxCode != 9){
-        config_change = true;
-        config.rxCode = 9;
-        config.prf = DWT_PRF_64M;
-      }
-      if (config.rxPAC != PAC){
-        config_change = true;
-        config.rxPAC = PAC;
-      }
-      if (config.sfdTO != SFD_TO){
-        config_change = true;
-        config.sfdTO = SFD_TO;
-      }
-      if (config_change){
-        dwt_configure(&config);
-      }
-      dwt_forcetrxoff();
-      dwt_rxreset();
-      dwt_rxenable(DWT_START_RX_IMMEDIATE);
-      P2_timeout = 0;
-      CLS_timeout = 0;
-    }
-    if (detection_status == RX_WAK_P2){
-      config_change = false;
-      if (config.rxCode != 5){
-        config_change = true;
-        config.rxCode = 5;
-        config.prf = DWT_PRF_16M;
-      }
-      if (config.rxPAC != PAC){
-        config_change = true;
-        config.rxPAC = PAC;
-      }
-      if (config.sfdTO != SFD_TO){
-        config_change = true;
-        config.sfdTO = SFD_TO;
-      }
-      if (config_change){
-        dwt_configure(&config);
-      }
-      etimer_set(&et, RAPID_SNIFF_INTERVAL - 1);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et));
-      P2_timeout += RAPID_SNIFF_INTERVAL;
-      if (P2_timeout >= P2_TO_THRESH){
-        dwt_forcetrxoff();
-        dwt_rxreset();
-        detection_status = RX_WAK_P1;
-      }
-      dwt_forcetrxoff();
-      dwt_rxreset();
-      dwt_rxenable(DWT_START_RX_IMMEDIATE);
-    }
-#if(TS_MODE == 1)
-    if (detection_status == RX_WAK_P3){
-      config.prf = DWT_PRF_64M;
-      config.rxCode = 9;
-      config.rxPAC = DWT_PAC32;
-      config.sfdTO = 8000;
-      dwt_configure(&config);
-      dwt_setpreambledetecttimeout(0);
-      dwt_forcetrxoff();
-      if(dwt_rxenable(DWT_START_RX_IMMEDIATE) != DWT_SUCCESS){
-        printf("NOT OK\n");
-      }
-      detection_status = WAITING_TS;
-      printf("Waiting for TS MSG\n");
-    }
-    if (detection_status == WAITING){
-      etimer_set(&et, 2 * (node_id % 100) + 2);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et));
-      detection_status = RDY_TO_TX;
-    }
-#else
-    if (detection_status == WAITING){
-      config.prf = DWT_PRF_64M;
-      config.rxCode = 9;
-      config.rxPAC = DWT_PAC32;
-      config.sfdTO = 8000;
-      dwt_configure(&config);
-      dwt_setpreambledetecttimeout(0);
-      
-      etimer_set(&et, RAPID_SNIFF_INTERVAL + 2);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et));
-#if (CCA_EN == 1)
-      detection_status = CCA;
-#else
-      detection_status = RDY_TO_TX;
-#endif
-    }
-
-    if (detection_status == CCA){
-      printf("Starting CCA\n");
-      unsigned short r = 3 +  (random_rand() % 3);
-      dwt_rxenable(DWT_START_RX_IMMEDIATE);
-      etimer_set(&et, r);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et));
-      if (detection_status == PKT_DETECTED){
-        detection_status = CCA;
-      }else{
-        detection_status = RDY_TO_TX;
-      }
-    }
-#endif
-    if (detection_status == RDY_TO_TX){
-      detection_status = RX_WAK_P1;
-      config.rxPAC = PAC;
-      config.prf = DWT_PRF_64M;
-      config.rxCode = 9;
-      config.sfdTO = SFD_TO;
-      dwt_configure(&config);
-      dwt_forcetrxoff();
-      dwt_writetxdata(sizeof(payload), payload, 0);
-      dwt_writetxfctrl(sizeof(payload), 0, 0);
-      printf("TX: %d\n", node_id);
-      if(dwt_starttx(DWT_START_TX_IMMEDIATE) != DWT_SUCCESS){
-        printf("TX ERR\n");
-
-        printf("oh no\n");
-      }
-      etimer_set(&et, 200);
-      PROCESS_WAIT_UNTIL(etimer_expired(&et));
-      dwt_forcetrxoff();
-      dwt_setpreambledetecttimeout(PDTO);
-    }
-
+    dwt_writetxfctrl(sizeof(payload), 0, 0);
+    dwt_starttx(DWT_START_TX_IMMEDIATE);
+    printf("ADV sent\n");
   }
   
   PROCESS_END();
