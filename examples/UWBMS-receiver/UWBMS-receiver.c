b72ef834267de9c083c1ad8448e6de93255cacfb
alireza <alirezaasnaripour73@gmail.com>
2024-08-16 03:27:22 -0500

    before switching branches

--- examples/UWBMS-receiver/UWBMS-receiver.c
+++ examples/UWBMS-receiver/UWBMS-receiver.c
@@ -28,7 +28,116 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-#include "callbacks.h"
+
+
+
+
+#include "contiki.h"
+#include "lib/random.h"
+#include "net/rime/rime.h"
+#include "leds.h"
+#include "net/netstack.h"
+#include <stdio.h>
+#include "dw1000.h"
+#include "dw1000-ranging.h"
+#include <sys/node-id.h>
+#include "net/netstack.h"
+#include "core/net/linkaddr.h"
+/*---------------------------------------------------------------------------*/
+PROCESS(range_process, "Test range process");
+AUTOSTART_PROCESSES(&range_process);
+#define UUS_TO_DWT_TIME           65536
+#define TS_WAIT                   0
+/*---------------------------------------------------------------------------*/
+typedef struct {
+  uint8_t packet_type;
+  uint16_t src;
+  uint16_t dst;
+  uint32_t seq;
+  uint8_t payload[1200];
+} packet_t;
+
+typedef struct{
+  uint8_t sender_id;
+  uint8_t config_id;
+} time_sync_payload;
+
+typedef struct{
+  uint8_t sender_id;
+  uint32_t ts_seq;
+} data_payload;
+
+typedef struct{
+  uint8_t rxCode;
+  uint8_t ts_rxCode;
+  uint16_t rx_wait_us;
+} rx_info_t;
+
+
+/*---------------------------------------------------------------------------*/
+packet_t rxpkt;
+
+dwt_config_t config =   {
+    5, /* Channel number. */
+    DWT_PRF_64M, /* Pulse repetition frequency. */
+    DWT_PLEN_1024, /* Preamble length. Used in TX only. */
+    DWT_PAC8, /* Preamble acquisition chunk size. Used in RX only. */
+    9, /* TX preamble code. Used in TX only. */
+    9, /* RX preamble code. Used in RX only. */
+    1, /* 0 to use standard SFD, 1 to use non-standard SFD. */
+    DWT_BR_6M8, /* Data rate. */
+    DWT_PHRMODE_EXT, /* PHY header mode. */
+    (8000 + 1 + 64 - 64) /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+};
+int bytes_received = 0;
+
+uint32_t last_seq = 0;
+uint32_t start_seq = 0;
+
+int packets_received = 0;
+rx_info_t rx_info;
+/*---------------------------------------------------------------------------*/
+
+void rx_ok_cb(const dwt_cb_data_t *cb_data){
+  dwt_readrxdata((uint8_t *) &rxpkt, 20, 0);
+  uint32_t rx_timestamp;
+  uint32_t tx_time;
+  rx_timestamp = dwt_readrxtimestamphi32();
+  dwt_forcetrxoff();
+  
+  // if (rxpkt.seq - last_seq != 1)
+  //   printf("Diff: %d, %d\n", rxpkt.seq, rxpkt.seq - last_seq);
+  if (rxpkt.packet_type == 1){
+    config.rxCode = rx_info.rxCode;
+    dwt_configure(&config);
+    dwt_setpreambledetecttimeout(1000);
+    dwt_rxenable(DWT_START_RX_IMMEDIATE);
+    printf("TS received\n");
+  }
+  
+  if (rxpkt.packet_type == 2){
+    last_seq = rxpkt.seq;
+    packets_received++;
+    dwt_rxenable(DWT_START_RX_IMMEDIATE);
+    printf("DA RX\n");
+  }
+  
+}
+
+void rx_to_cb(const dwt_cb_data_t *cb_data){
+  config.rxCode = rx_info.ts_rxCode;
+  dwt_configure(&config);
+  dwt_setpreambledetecttimeout(0);
+  dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  printf("TO\n");
+}
+
+void rx_err_cb(const dwt_cb_data_t *cb_data){
+  dwt_forcetrxoff();
+  dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  printf("RX ERR: %d\n", cb_data->status);
+}
+
 
 
 PROCESS_THREAD(range_process, ev, data)
@@ -70,7 +179,7 @@ PROCESS_THREAD(range_process, ev, data)
   rx_info.ts_rxCode = config.rxCode;
   config.rxCode = rx_info.rxCode;
 #endif
-
+  printf("RX code %d\n", config.rxCode);
   dwt_configure(&config);
   bytes_received = 0;
   dwt_forcetrxoff();
@@ -78,13 +187,13 @@ PROCESS_THREAD(range_process, ev, data)
   while (1){
     etimer_set(&et, CLOCK_SECOND);
     PROCESS_WAIT_UNTIL(etimer_expired(&et));
-    if (last_seq != 0){
-      printf("PRR: %d\n", (100 * packets_received) / (last_seq - start_seq));
-      start_seq = last_seq;
-      packets_received = 0;
-    }else{
-      printf("No data yet!!!\n");
-    }
+    // if (last_seq != 0){
+    //   printf("PRR: %d\n", (100 * packets_received) / (last_seq - start_seq));
+    //   start_seq = last_seq;
+    //   packets_received = 0;
+    // }else{
+    //   printf("No data yet!!!\n");
+    // }
   }
   PROCESS_END();
 }
--- examples/UWBMS-sender/UWBMS-sender.c
+++ examples/UWBMS-sender/UWBMS-sender.c
@@ -28,10 +28,144 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-#include "callbacks.h"
+#include "contiki.h"
+#include "lib/random.h"
+#include "net/rime/rime.h"
+#include "leds.h"
+#include "net/netstack.h"
+#include <stdio.h>
+#include "dw1000.h"
+#include "dw1000-ranging.h"
+#include "core/net/linkaddr.h"
+#include <sys/node-id.h>
+#include "net/netstack.h"
+
+
+
+#define STM32_UUID ((uint32_t *)0x1ffff7e8)
+#define CALLBACKS_H_
+/*---------------------------------------------------------------------------*/
+
+#define FRAME_SIZE          1022
+#define PACKET_TS           1
+#define PACKET_DA           2
+#define UUS_TO_DWT_TIME     65536
+#define TS_WAIT             0
+
+#define UUS_TO_DWT_TIME 65536
+
+
+/*---------------------------------------------------------------------------*/
+typedef struct {
+  uint8_t packet_type;
+  uint16_t src;
+  uint16_t dst;
+  uint32_t seq;
+  uint8_t payload[1200];
+} packet_t;
+
+typedef struct{
+  uint8_t sender_id;
+  uint8_t config_id;
+} time_sync_payload;
+
+typedef struct{
+  uint8_t sender_id;
+  uint32_t ts_seq;
+} data_payload;
+
+typedef struct{
+  uint8_t tx_PC;
+  uint16_t packet_len;
+  uint16_t tx_IPI_ms;
+  uint16_t tx_wait_us;
+  uint16_t tx_packet_num;
+  uint32_t end_seq;
+} sender_info_t;
+
+/*---------------------------------------------------------------------------*/
+
+packet_t rxpkt;
+packet_t txpkt;
+sender_info_t instance_info;
+
+static struct etimer et;
+
+dwt_config_t config =   {
+    5, /* Channel number. */
+    DWT_PRF_64M, /* Pulse repetition frequency. */
+    DWT_PLEN_128, /* Preamble length. Used in TX only. */
+    DWT_PAC32, /* Preamble acquisition chunk size. Used in RX only. */
+    9, /* TX preamble code. Used in TX only. */
+    9, /* RX preamble code. Used in RX only. */
+    1, /* 0 to use standard SFD, 1 to use non-standard SFD. */
+    DWT_BR_6M8, /* Data rate. */
+    DWT_PHRMODE_EXT, /* PHY header mode. */
+    (8000 + 1 + 64 - 64) /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+};
+
+dwt_txconfig_t txConf = {
+    0xC9, //PGDelay
+    0x18181818 //30 dB
+};
+
+void rx_ok_cb(const dwt_cb_data_t *cb_data){
+  uint32_t rx_time = dwt_readrxtimestamphi32();
+  dwt_forcetrxoff();
+  dwt_readrxdata((uint8_t *) &rxpkt, cb_data->datalength, 0);
+  uint32_t tx_time = rx_time + ((UUS_TO_DWT_TIME * instance_info.tx_wait_us) >> 8);
+  dwt_setdelayedtrxtime(tx_time);
+  dwt_writetxdata(20, (uint8_t *) &txpkt, 0);
+  dwt_writetxfctrl(instance_info.packet_len, 0, 0);
+
+  if (rxpkt.packet_type == PACKET_TS){
+    instance_info.end_seq = txpkt.seq + instance_info.tx_packet_num;
+    dwt_writetxfctrl(FRAME_SIZE, 0, 0);
+    if (dwt_starttx(DWT_START_TX_DELAYED) == DWT_SUCCESS){
+      printf("TS Frame: %d\n", rxpkt.seq);
+    }else{
+      printf("TX failed \n");
+    }
+    
+  }else{
+    printf("Something else\n");
+  }
+  
+}
+
+void tx_ok_cb(const dwt_cb_data_t *cb_data){
+  txpkt.seq++;
+#if TS_WAIT == 1
+  if (txpkt.seq < instance_info.end_seq){
+    dwt_forcetrxoff();
+    dwt_writetxdata(20, (uint8_t *) &txpkt, 0);
+    dwt_writetxfctrl(instance_info.packet_len, 0, 0);
+    dwt_starttx(DWT_START_TX_IMMEDIATE);
+  }else{
+    dwt_forcetrxoff();
+    dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  }
+  printf("TX done\n");
+#else
+  dwt_forcetrxoff();
+  dwt_writetxfctrl(instance_info.packet_len, 0, 0);
+  dwt_starttx(DWT_START_TX_IMMEDIATE);
+  dwt_writetxdata(20, (uint8_t *) &txpkt, 0);
+  printf("TX done NS\n");
+#endif
+}
+
+void rx_err_cb(const dwt_cb_data_t *cb_data){
+  dwt_forcetrxoff();
+  dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  printf("RXERR\n");
+}
+
 
 PROCESS(range_process, "Test range process");
 AUTOSTART_PROCESSES(&range_process);
+
+
 
 PROCESS_THREAD(range_process, ev, data)
 {
@@ -42,12 +176,12 @@ PROCESS_THREAD(range_process, ev, data)
   PROCESS_BEGIN();
   
   dwt_setcallbacks(&tx_ok_cb, rx_ok_cb, NULL, &rx_err_cb);
-  uint16_t node_id = 0;
-  // if(deployment_set_node_id_ieee_addr()){
-  //   printf("NODE addr set successfully: %d\n", node_id);
-  // }else{
-  //   printf("Failed to set nodeID\n");
-  // }
+
+  if(deployment_set_node_id_ieee_addr()){
+    printf("NODE addr set successfully: %d\n", node_id);
+  }else{
+    printf("Failed to set nodeID\n");
+  }
   instance_info.packet_len = 100;
   instance_info.tx_IPI_ms = 0;
 
